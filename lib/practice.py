f29e6c0300c06b9ffb175824fcf792ad0bd329b1
Justin Bass <justinbass6036@gmail.com>
2024-09-11 18:37:33 -0400

    Createda function where I use a list comprehension to find and return all even numbers through an argument of a given list of ints

---
+++ lib/practice.py
@@ -0,0 +1,22 @@
+##List Comprehensions - Certain lists are very straightforward to create- some are even passed into your
+#code as parameters and don't require any work at all. Others take a bit more effort:
+
+#squared minus one without list comprehension
+squared_minus_one = list()
+
+for n in range(1, 11): #range(1, 11) will give me 1,2,3,4,5,6,7,8,9,10 - my for in will loop 10 timea
+    squared_minus_one.append((n ** 2) - 1)
+print(squared_minus_one)
+
+#squared_minus_1 with list comprehension
+squared_minus_1 = [(n ** 2) - 1 for n in range(1, 11)]
+print(squared_minus_1)
+
+
+#find all even numbers in list with list comprehension
+def even_nums(num_list):
+    num_list = [n for n in num_list if n % 2 == 0]
+    print(num_list)
+
+even_nums([1,2,3,4,5,6])
+
